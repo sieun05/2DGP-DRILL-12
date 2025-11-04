24f34287fa3052895d6747a21f295e4833de3890
OhSieun <sieun125@tukorea.ac.kr>
2025-11-04 21:56:21 +0900

    새 10마리 game_world에 추가 윈도우 범위 밖으로 넘어갔을 때 끼는 오류 확인

--- play_mode.py
+++ play_mode.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import random
 
 from boy import Boy
 from grass import Grass
@@ -34,8 +35,8 @@ def init():
     boy = Boy()
     game_world.add_object(boy, 1)
 
-    bird = Bird()
-    game_world.add_object(bird, 1)
+    birds = [Bird(random.randint(1, 1599)) for i in range(10)]
+    game_world.add_objects(birds, 1)
 
 def update():
     game_world.update()
